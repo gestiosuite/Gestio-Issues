ba7ffff3616a209ce1cecebda4fb56cdd035bb35
jawittdesigns <jawittdesigns@gmail.com>
2014-06-01 07:43:24 -0400

    Added GitHub URL

--- gestio-issues.php
+++ gestio-issues.php
@@ -10,7 +10,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * GitHub Plugin URI: @TODO
+ * GitHub Plugin URI: https://github.com/gestiosuite/Gestio-Issues
  */
 
 // If this file is called directly, abort.
